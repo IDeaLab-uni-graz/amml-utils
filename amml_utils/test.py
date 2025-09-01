8f95e91cfeea44bcaee4885b3542f75d379d5629
Hendrik Kleikamp <hendrik.kleikamp@uni-graz.at>
2025-09-01 14:11:06 +0200

    improved test file (also loading full dataset now)

--- amml_utils/test.py
+++ amml_utils/test.py
@@ -7,4 +7,7 @@ print(f"Available datasets: {list_datasets()}")
 
 data_loader = get_data_loader("EXAMPLE", data_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), "data"))
 tensor = data_loader.get_dataset_as_tensor("train")
-print(tensor.shape)
+print(f"Shape of training set loaded as tensor: {tensor.shape}")
+
+dataset = data_loader.get_dataset()
+print(f"Length of full dataset: {len(dataset)}")
